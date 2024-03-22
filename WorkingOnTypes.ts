05cf31493d142b46904358ef433512cae0ef0197
The Nuraryhion <zadruzynskimateusz@gmail.com>
2024-03-22 14:00:46 +0100

    Łączenie typów i deklaracje

---
+++ WorkingOnTypes.ts
@@ -0,0 +1,21 @@
+
+const noOne = 5;
+//const noTwo: number = 7; <-- nie deklarować, że zmienna jest danego typu, jeżeli jest zainicjowana z wartością od razu typ wartości jest rozpoznawany przy deklaracji wartości
+let noTwo: number; //<-- jeżeli zmienna jest zainicjowana ale bez wartości, możemy określić jej typ
+const showResult = true;
+const prefix = "Wynik dodawania";
+
+function Add(variableOne:number, variableTwo:number, passResult:boolean, prefix:string)
+{
+    if(passResult)
+    {
+        var additionResult = variableOne + variableTwo
+        console.log(prefix + " to: " + additionResult);
+        //console.log(prefix + " to: " + variableOne + variableTwo); <-- taka struktóra zamienia wszystko na stringi czyli 57 nie 12
+    }
+    else
+        console.log(prefix + " został ukryty.");
+}
+
+noTwo = 7;
+Add(noOne,noTwo,showResult,prefix);
