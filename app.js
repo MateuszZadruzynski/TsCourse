f1bc1ea892dcc101dc678a977f3e635138eab9ea
The Nuraryhion <zadruzynskimateusz@gmail.com>
2024-03-22 12:54:31 +0100

    Typy Liczbowe

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello i am done!");
+function Add(numberOne, numberTwo) {
+    return numberOne + numberTwo;
+}
+var numberOne = 2;
+var numberTwo = 5;
+var result = Add(numberOne, numberTwo);
+console.log("Wynik to: " + result);
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log("Hello i am done!")
+function Add(numberOne:number, numberTwo:number,)
+{
+    return numberOne + numberTwo;
+}
+
+const numberOne = 6;
+const numberTwo = 5;
+
+const result = Add(numberOne,numberTwo);
+console.log("Wynik to: " + result);
