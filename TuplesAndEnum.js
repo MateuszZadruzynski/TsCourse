923942322b68acc5c538e1cd2bab4f2592f98eff
The Nuraryhion <zadruzynskimateusz@gmail.com>
2024-03-27 09:56:05 +0100

    TuplesAndEnum part 2

--- TuplesAndEnum.js
+++ TuplesAndEnum.js
@@ -1,12 +1,22 @@
-var PC;
-PC = {
-    ram: [3, "8GB"],
+// let PC:{
+//     ram: [number, string,RAM_Type],
+//     gpu: [number, string],
+//     ssd: [number, string],
+//     extensions: [number, string[]]
+// }
+var RAM_Type;
+(function (RAM_Type) {
+    RAM_Type[RAM_Type["DDR3"] = 3] = "DDR3";
+    RAM_Type[RAM_Type["DDR4"] = 4] = "DDR4";
+    RAM_Type[RAM_Type["DDR5"] = 5] = "DDR5";
+})(RAM_Type || (RAM_Type = {}));
+var PC = {
+    ram: [3, "8GB", RAM_Type.DDR5],
     gpu: [1, "RTX4040"],
     ssd: [2, "1T"],
     extensions: [4, ['Bluetooth', 'Sound card', 'USB port', 'WI-FI card']]
 };
-console.log(PC);
-console.log("\nChanging RAM");
+console.log("\nChanging RAM\n");
 PC.ram[0] = 2;
 PC.ram[1] = "16GB";
-console.log("\n" + PC);
+console.log(PC);
--- TuplesAndEnum.ts
+++ TuplesAndEnum.ts
@@ -1,21 +1,25 @@
-let PC:{
-    ram: [number, string],
-    gpu: [number, string],
-    ssd: [number, string],
-    extensions: [number, string[]]
+// let PC:{
+//     ram: [number, string,RAM_Type],
+//     gpu: [number, string],
+//     ssd: [number, string],
+//     extensions: [number, string[]]
+// }
+
+enum RAM_Type {
+    DDR3 = 3.0,
+    DDR4 = 4.0,
+    DDR5 = 5.0
 }
 
- PC = {
-    ram: [3,"8GB"],
+ const PC = {
+    ram: [3,"8GB",RAM_Type.DDR5],
     gpu: [1, "RTX4040"],
     ssd: [2, "1T"],
     extensions: [4, ['Bluetooth', 'Sound card', 'USB port', 'WI-FI card']]
 }
 
-console.log(PC);
-
-console.log("\nChanging RAM");
+console.log("\nChanging RAM\n");
 PC.ram[0] = 2;
 PC.ram[1] = "16GB";
 
-console.log("\n" + PC);
+console.log(PC);
