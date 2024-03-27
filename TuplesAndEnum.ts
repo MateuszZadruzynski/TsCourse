a34ef8219c3e5968287041bbc4511e29c6220240
The Nuraryhion <zadruzynskimateusz@gmail.com>
2024-03-27 09:42:52 +0100

    Tuples

---
+++ TuplesAndEnum.ts
@@ -0,0 +1,21 @@
+let PC:{
+    ram: [number, string],
+    gpu: [number, string],
+    ssd: [number, string],
+    extensions: [number, string[]]
+}
+
+ PC = {
+    ram: [3,"8GB"],
+    gpu: [1, "RTX4040"],
+    ssd: [2, "1T"],
+    extensions: [4, ['Bluetooth', 'Sound card', 'USB port', 'WI-FI card']]
+}
+
+console.log(PC);
+
+console.log("\nChanging RAM");
+PC.ram[0] = 2;
+PC.ram[1] = "16GB";
+
+console.log("\n" + PC);
