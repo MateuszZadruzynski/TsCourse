a0a8c00dc130a9fc9d3bcd2657c7ea56bbfb2d2a
The Nuraryhion <zadruzynskimateusz@gmail.com>
2024-03-28 13:49:33 +0100

    Functions and Typesd part.2

--- Functions.js
+++ Functions.js
@@ -4,4 +4,14 @@ function add(firstNumber, secondNumber) {
 function printResult(num) {
     console.log('Result ' + num);
 }
+function addAndHandle(an, bn, cb) {
+    var result = an + bn;
+    cb(result);
+}
 printResult(add(10, 17));
+var combinedValues; //przyjmuje dwa parametry musi być funckjną i zwracać liczbę
+combinedValues = add;
+console.log(combinedValues(5, 5));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
